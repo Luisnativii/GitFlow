f71313d5a52b816d89a33bc9e12bca5b95048b72
Luisnativii <00200022@uca.edu.sv>
2024-05-02 22:01:31 -0600

    Commit Inicial

---
+++ UCALifeS01/app/src/main/java/com/evarela/ucalifes01/component/BottonNavBar/NavBarItemList.kt
@@ -0,0 +1,25 @@
+package com.evarela.ucalifes01.component.BottonNavBar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.evarela.ucalifes01.R
+
+@Composable
+fun NavBarItemList(): List<NavBarItem>{
+    return listOf(
+        NavBarItem(
+            "Home",
+            ScreenRoute.Home.route,
+            ImageVector.vectorResource(id = R.drawable.ic_home_filled),
+            ImageVector.vectorResource(id = R.drawable.ic_home_outlined)
+        ),
+        NavBarItem(
+            "Add",
+            ScreenRoute.Add.route,
+            ImageVector.vectorResource(id = R.drawable.ic_add_filled),
+            ImageVector.vectorResource(id = R.drawable.ic_add_outlined)
+        ),
+
+    )
+}
